4c4c2028abfb4c578234a96ba04760c3b4d6c21c
Hankyo <walner.martin@web.de>
2018-07-15 15:34:44 +0200

    .

--- keyboards/planck/keymaps/Hankyo/rules.mk
+++ keyboards/planck/keymaps/Hankyo/rules.mk
@@ -1,2 +1,15 @@
 AUTO_SHIFT_ENABLE = yes
 AUTO_SHIFT_MODIFIERS = yes
+
+
+
+BOOTMAGIC_ENABLE = no   # virtual DIP switch configuration(+1000)
+EXTRAKEY_ENABLE  = yes   # audio control and System control(+450)
+#CONSOLE_ENABLE   = yes   # console for debug(+400)
+#COMMAND_ENABLE   = yes   # commands for debug and configuration
+BACKLIGHT_ENABLE = no   # enable keyboard backlight functionality
+MIDI_ENABLE      = no   # MIDI controls
+#AUDIO_ENABLE     = no   # audio output on port C6
+UNICODE_ENABLE   = no   # unicode
+BLUETOOTH_ENABLE = no   # enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE  = no   # enable WS2812 RGB underlight, do not enable this with audio at the same time.
