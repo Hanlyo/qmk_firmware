83e48a6eff632de2f5eac9f8b3fd9f71a9fdecc4
Hankyo <walner.martin@web.de>
2018-07-18 22:12:22 +0200

    Und wieder zurueck.

--- keyboards/planck/keymaps/Hankyo/keymap.c
+++ keyboards/planck/keymaps/Hankyo/keymap.c
@@ -258,6 +258,7 @@ uint32_t layer_state_set_user(uint32_t state) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
 
+/*
     case LOWER:
       if (record->event.pressed) {
         layer_off(_RAISE);
@@ -270,7 +271,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
         break;
-
+*/
 
 
     case QWERTY:
