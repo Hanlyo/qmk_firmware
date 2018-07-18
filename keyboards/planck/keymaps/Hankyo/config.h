27e71b7db3346210cdd7399313a45b326c498fc2
Hankyo <walner.martin@web.de>
2018-07-18 20:25:07 +0200

    .

--- keyboards/planck/keymaps/Hankyo/config.h
+++ keyboards/planck/keymaps/Hankyo/config.h
@@ -40,25 +40,23 @@
 
 
 
-
-
-
-
-
-
 // my custom configurations
 
 
+// Autoshift stuff
 
-#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT 140
 #define NO_AUTO_SHIFT_SPECIAL
 
 
+// Mouse stuff
 
-
-
-
-
+#define MOUSEKEY_DELAY             300
+#define MOUSEKEY_INTERVAL          50
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
 
 
