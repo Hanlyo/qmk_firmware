d7cd299cdefba94f7b639336ac57223aa38a6222
Hankyo <walner.martin@web.de>
2018-07-18 21:45:59 +0200

    Mouse speed und so etwas angepast. Taptoggel angepasst. Und ein paar andere Tasten angepasst.

--- keyboards/planck/keymaps/Hankyo/config.h
+++ keyboards/planck/keymaps/Hankyo/config.h
@@ -51,15 +51,17 @@
 
 // Mouse stuff
 
-#define MOUSEKEY_DELAY             200
+#define MOUSEKEY_DELAY             170
 #define MOUSEKEY_INTERVAL          50
 #define MOUSEKEY_MAX_SPEED         5
-#define MOUSEKEY_TIME_TO_MAX       10
+#define MOUSEKEY_TIME_TO_MAX       8
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
 
+// Taptoggle stuff
 
+#define TAPPING_TOGGLE 2
 
 
 
