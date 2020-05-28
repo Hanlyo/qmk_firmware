cb8ddf0c4d885c46172ba8809ef8fc637af82613
Hankyo <walner.martin@web.de>
2020-05-28 21:17:19 +0200

    spacefn ausgebaut

--- keyboards/planck/keymaps/Hankyo/keymap.c
+++ keyboards/planck/keymaps/Hankyo/keymap.c
@@ -64,7 +64,7 @@ enum planck_layers {
 //  _PLOVER,
   _ADJUST,
   _FN,
-  _SPACE_FN
+//  _SPACE_FN
 };
 
 enum planck_keycodes {
@@ -83,7 +83,7 @@ enum planck_keycodes {
 #define RAISE TT(_RAISE)
 //#define MOUSE TG(_MOUSE)
 #define FN MO(_FN)
-#define SPACE_FN LT(_SPACE_FN, KC_SPC)
+//#define SPACE_FN LT(_SPACE_FN, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL },
   {KC_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    N_QUES , N_SLSH },
   {TD(X_CTL),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_ENT)},
-  {KC_LCTL,  KC_LALT, KC_LGUI, FN,      LOWER,   SPACE_FN,KC_BSPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_LCTL,  KC_LALT, KC_LGUI, FN,      LOWER,   /*SPACE_FN*/KC_SPC,KC_BSPC, RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 
@@ -214,13 +214,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |       |      |      |      |      |             |      | Next |VOLDWN|VOL UP| Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_SPACE_FN] = {
+/*[_SPACE_FN] = {
     {XXXXXXX,  KC_PGUP, KC_BSPC,  KC_UP,     KC_DEL,   KC_PGDN,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX},
     {XXXXXXX,  KC_HOME, KC_LEFT,  KC_DOWN,   KC_RGHT,  KC_END,   XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX},
     {XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   KC_ENT,   XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX},
     {XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,  XXXXXXX,  XXXXXXX, KC_MNXT,  KC__VOLDOWN, KC__VOLUP,  KC_MPLY}
 },
-
+*/
 
 /* Mouse
  * ,-----------------------------------------------------------------------------------.
