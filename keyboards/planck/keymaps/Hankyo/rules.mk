97923977f63b1b2c7abe99490d07e2364ae0c449
Hankyo <walner.martin@web.de>
2020-05-28 21:42:07 +0200

    vllt

--- keyboards/planck/keymaps/Hankyo/rules.mk
+++ keyboards/planck/keymaps/Hankyo/rules.mk
@@ -2,13 +2,13 @@
 
 
 
-TAP_DANCE_ENABLE    = no   # Enable TapDance functionality (+100)
+TAP_DANCE_ENABLE    = yes   # Enable TapDance functionality (+100)
 BOOTMAGIC_ENABLE    = no    # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE     = no    # Mouse keys(+4700)
 EXTRAKEY_ENABLE     = yes    # Audio control and System control(+450)
 CONSOLE_ENABLE      = no    # Console for debug(+400)
 COMMAND_ENABLE      = no    # Commands for debug and configuration
-NKRO_ENABLE         = yes   # Nkey Rollover (+300) - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE         = no   # Nkey Rollover (+300) - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE    = no    # Enable keyboard backlight functionality
 MIDI_ENABLE         = no    # MIDI controls
 AUDIO_ENABLE        = yes    # Audio output on port C6 (+9400)
